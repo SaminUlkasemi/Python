977398e5889bd951f6efb68f02aff27e0c77d922
SaminUlkasemi <144313070+SaminUlkasemi@users.noreply.github.com>
2024-07-16 17:02:48 +0600

    Flow control

--- 0005_flow_control.py
+++ 0005_flow_control.py
@@ -1,3 +1,5 @@
+############################## If else #############################
+
 num = 10
 #  Be mindful of the indentation while writing the if statements. Indentation is the whitespace at 
 #  the beginning of the code.
@@ -24,5 +26,33 @@ grade = 83
 resut = "pass" if grade > 55 else "fail"
 print(resut)
 
+# Multiple condition
 if num > 5 and num < 15:
     print("Num greater than 5 less than 15")
+    
+    
+    
+############################## Loops #############################
+os_list = ["Linux", "Windows", "Mac"]
+for os in os_list:
+    print(os)
+    
+langugage = "Systemverilog"
+for char in langugage:
+    print(char)
+    
+
+# For loop range
+for i in range(5):
+    print(i)
+    
+# For loop with else
+for i in range(2):
+    print(i)
+else :
+    print("No item left")
+    
+# Usse "_" as placehold and its value is intentionally being ignored.
+for _ in range(5):
+    print("Execute")
+    
--- garbase/function_multipy.py
+++ garbase/function_multipy.py
@@ -2,3 +2,6 @@ def func(a, b):
     return a*b
     
 print(func(5, "10"))
+
+
+  print(f"i = {i}, j = {j}")
