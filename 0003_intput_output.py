d3070d46e9958a9d63671717a1ce73ef609d3029
SaminUlkasemi <144313070+SaminUlkasemi@users.noreply.github.com>
2024-07-16 15:30:14 +0600

    add input output operator

---
+++ 0003_intput_output.py
@@ -0,0 +1,13 @@
+########################    Python Output    ########################
+output_file = "flash.txt"
+
+# object - value(s) to be printed
+# sep (optional) - allows us to separate multiple objects inside print().
+# end (optional) - allows us to add add specific values like new line "\n", tab "\t"
+# file (optional) - where the values are printed. It's default value is sys.stdout (screen)
+# flush (optional) - boolean specifying if the output is flushed or buffered. Default: False
+# print("Ulkasemi", sep=" | ", file=output_file, flush=True)
+
+########################    Python Input    ########################
+name = input("Enter a name ")
+print("Your Entered: ", name)
---
+++ 0004_operator.py
@@ -0,0 +1,22 @@
+#####################   Arithmatic  #####################
+num1 = 10
+num2 = 5.5
+
+# Addition
+print("num1 + num2 : ", num1 + num2)
+# Substraction
+print("num1 - num2 : ", num1 - num2)
+# Division
+print("num1 / num2 : ", num1 / num2)
+# Floor division
+print("num1 // num2: ", num1 // num2)
+# Module
+print("num1 % num2 : ", num1 % num2)
+# Power
+print("num1 ** num2: ", num1**num2)
+
+
+####################    Logical ##########################
+print("(num1 > num2) and (num2 < num1): ", (num1 > num2) and (num2 < num1))
+print("(num1 > num2) or  (num2 > num1): ", (num1 > num2) or  (num2 > num1))
+print("not num1 > 20:", not num1 > 20)
