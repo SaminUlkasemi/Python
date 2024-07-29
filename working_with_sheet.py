7cbb9ef76f0afcf081f582b7f209f48c62b02022
SaminUlkasemi <144313070+SaminUlkasemi@users.noreply.github.com>
2024-07-29 11:30:05 +0600

    complete working with sheet

--- working_with_sheet.py
+++ working_with_sheet.py
@@ -5,7 +5,7 @@
 import random
 
 import openpyxl # type: ignore
-#* Remember that example.xlsx needs to be in the current working directory in
+#* Remember that example.xlsx needs to be in the current working directory in 
 #* order for you to work with it
 import os
 
@@ -59,8 +59,10 @@ for i in range(1, 6):
     item_list[f'A{i}'] = items[i-1]  
 
 for i in range(1, 6):
-    item_list[f'B{i}'] = f"{random.randint(5, 100)} BDT"
+    item_list[f'B{i}'] = random.randint(5, 100)
 
+for i in range(1, 6):
+    item_list[f'C{i}'] = "BDT"
 
 
 # get the value of a cell
@@ -68,13 +70,14 @@ val_b3 = item_list['B3'].value
 print(val_b3)
 
 # access using row column
-item_list.cell(row=4, column=2, value="1111 BDT")
+item_list.cell(row=4, column=2, value=1111)
 
 # accessing range of cells
 #! find out how to access value using this tuple, what are the methods avaiable
-print(item_list['B1':'B5']) # return tuple
+print("item_list['B1':'B5']: ", item_list['B1':'B5']) # return tuple
 
 # access values
+print("access values in row: ".center(70, "*"))
 for row in item_list.values:
     for val in row:
         print(val)
@@ -82,5 +85,45 @@ for row in item_list.values:
 for row in item_list.iter_rows(max_row=5, max_col=2, values_only=True):
     print(row)
 
-# ------------------------------ save the sheet ------------------------------ #
+
+# set value using cell
+c = item_list['B1']     # get the cell
+c.value = 130     # put the value
+
+print("Accessing row using cell".center(70, "*"))
+for i in range(1, 6):
+    print(f"{i}: ", item_list.cell(row=i, column=2).value)
+
+# ---------------------------------------------------------------------------- #
+#                                save the sheet                                #
+# ---------------------------------------------------------------------------- #
+#! Warning This operation will overwrite existing files without warning.
 wb.save("items.xlsx")
+#? ------------------------- save Workbook as template ------------------------ #
+wb.template =  True
+wb.save('example_template.xltx')
+
+
+# ---------------------------------------------------------------------------- #
+#                             Opening an excel file                            #
+# ---------------------------------------------------------------------------- #
+print(" Opening a excel file ".center(70, "*"))
+from openpyxl import load_workbook
+
+# opening the excel
+try:
+    wb = load_workbook(filename='items.xlsx')
+except Exception as e:
+    print(e)
+
+# sheetnames
+print("sheetnames: ", wb.sheetnames)
+
+listSheet = wb['list']
+print("listSheet: ", listSheet)
+print("listSheet type: ", type(listSheet))
+print("listSheet title: ", listSheet.title)
+
+print(listSheet['A1'].value, listSheet['B1'].value, listSheet['C1'].value)
+print("listSheet['A1'].row: ", listSheet['A1'].row.value)
+print("listSheet['A1'].column: ", listSheet['A1'].column)
